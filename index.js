ab45b28539605599f4ba5b289ca826058086d3ed
Daniel Interiano <dannyboyint@gmail.com>
2021-11-16 14:50:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 function createEmployeeRecord(array){
+    const record = {
+    firstName: "",
+    familyName: "",
+    title: "",
+    payPerHour: "",
+    }
+    record.firstName = array[0]
+    record.familyName = array[1]
+    record.title = array[2]
+    record.payPerHour = array[3]
+    record['timeInEvents'] = [];
+    record['timeOutEvents'] = [];
     
-    let project = Object.assign({}, array)
-    
-    project['timeInEvents'] = [];
-    project['timeOutEvents'] = [];
-    
-    console.log(project) 
+    return record
 }
 
+function createEmployeeRecords(array){
+    return array.map((item) => { return createEmployeeRecord(item)})
+}
+
+function createTimeInEvent(record, dateStamp){
+    let splitData = dateStamp.split(' ')
+    
+    let hourStamp = splitData[1]
+    let dayStamp = splitData[0]
+    
+    record.timeInEvents = {
+        type: "TimeIn",
+        hour: `${hourStamp}`,
+        date: `${dayStamp}`
+    }
+    
+    return record
+}
+
+function createTimeOutEvent(record, dateStamp){
+    let splitData = dateStamp.split(' ');
+    let hoursStamp = splitData[1];
+    let timeStamp = splitData[0]
+
+    record.timeOutEvents = {
+        type: "TimeOut",
+        hour: `${hoursStamp}`,
+        date: `${timeStamp}`
+    }
+
+    return record
+}
+
+
 /* 
-
-
+  const record = {
     firstName: "",
     familyName: "",
     title: "",
     payPerHour: "",
     timeInEvents: [],
     timeOutEvents: []
-
+    }
 */
