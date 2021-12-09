8673686801ee3fdbeca2212c87426e5771978889
Daniel Interiano <dannyboyint@gmail.com>
2021-12-09 09:24:28 -0500

    commit tree 1d2d0a3c7dcfa206105049f0d8dd304fad29e7c2

--- index.js
+++ index.js
@@ -1,49 +1,14 @@
-function createEmployeeRecord(array){
-    const record = {
-    firstName: "",
-    familyName: "",
-    title: "",
-    payPerHour: "",
+let createEmployeeRecord = function(row){
+    console.log(row)
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
     }
-    record.firstName = array[0]
-    record.familyName = array[1]
-    record.title = array[2]
-    record.payPerHour = array[3]
-    record['timeInEvents'] = [];
-    record['timeOutEvents'] = [];
-    
-    return record
 }
-
-function hoursWorkedOnDate(record, yearToDateStamp){
-    let timeInDateMatch = record.timeInEvents.find((object) => object['date'] === yearToDateStamp).hour;
-    let timeOutDateMatch = record.timeOutEvents.find((object) => object['date'] === yearToDateStamp).hour;
-   
-      return timeOutDateMatch/100 - timeInDateMatch/100
-  }
-
- function wagesEarnedOnDate(record, yearToDateStamp){
-    let hoursDated = hoursWorkedOnDate(record, yearToDateStamp)
-    return hoursDated * record.payPerHour
- } 
-
-function allWagesFor(record){
-    let dates = record.timeInEvents.map(function(calendar){return calendar.date})
-    
-    let wagesAccumulated = dates.reduce(function(one, date){return one + wagesEarnedOnDate(record, date)}, 0)
-
-    return wagesAccumulated;
-}
-
-function calculatePayroll(arrayRecord){
-   //using wagesEarnedOneDate, ACCUMULATE value of ALL DATES worked in RECORD
-   let employeesDates = arrayRecord.map(function(employee){return employee.timeInEvents.date})
-
-    //return the SUM of pay owed to ALL Employees for ALL DATES
-   return employeesDates.reduce(function(one, date){return one + wagesEarnedOnDate(recordObject, date)}, 0)
- 
-    }
-
 function createEmployeeRecords(array){
     return array.map((item) => { return createEmployeeRecord(item)})
 }
@@ -77,20 +42,37 @@ function createTimeOutEvent(record, dateStamp){
     return record
 }
 
-/*
-  const record = {
-    firstName: "Naruto",
-    familyName: "Uzumaki",
-    title: "Hokage",
-    payPerHour: "7",
-    timeInEvents: {
-        type: "TimeIn",
-        hour: 0900,
-        date: "0044-03-15"
-    },
-    timeOutEvents: {
-        type: "TimeOut",
-        hour: 1100,
-        date: "0044-03-15"
-    }
-    }*/
+function hoursWorkedOnDate(record, yearToDateStamp){
+    let timeInDateMatch = record.timeInEvents.find((object) => object.date === yearToDateStamp);
+    let timeOutDateMatch = record.timeOutEvents.find((object) => object.date === yearToDateStamp);
+   
+    return (timeOutDateMatch.hour - timeInDateMatch.hour)/100
+}
+
+function wagesEarnedOnDate(record, yearToDateStamp){
+    let hoursDated = hoursWorkedOnDate(record, yearToDateStamp)
+    * record.payPerHour
+    return parseFloat(hoursDated.toString())
+} 
+
+function allWagesFor(record){
+    let dates = record.timeInEvents.map(function(calendar){return calendar.date})
+    
+    let wagesAccumulated = dates.reduce(function(one, date){return one + wagesEarnedOnDate(record, date)}, 0)
+
+    return wagesAccumulated;
+}
+
+function calculatePayroll(array){
+   //using wagesEarnedOneDate, ACCUMULATE value of ALL DATES worked in RECORD
+    return array.reduce(function(one, date){ 
+        console.log(one)
+        console.log(date)
+        return one + allWagesFor(date)}, 0)
+
+
+    //return the SUM of pay owed to ALL Employees for ALL DATES
+    
+ 
+}
+
